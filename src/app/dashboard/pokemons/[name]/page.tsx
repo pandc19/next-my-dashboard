6abd122e469c9a666fe61f5170545821a8ea3ac0
Jonathan Pillajo <pandc19@outlook.com>
2024-01-14 21:53:06 -0500

    fin sección 6

--- src/app/dashboard/pokemons/[name]/page.tsx
+++ src/app/dashboard/pokemons/[name]/page.tsx
@@ -1,35 +1,29 @@
-import { Pokemon } from "@/pokemons";
+import { Pokemon, PokemonsResponse } from "@/pokemons";
 import { Metadata } from "next";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
 interface Props {
-    params: { id: string };
+    params: { name: string };
 }
 
 //! Se ejecuta en build time
 export async function generateStaticParams() {
+    const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+        .then(res => res.json());
 
-    const static151Pokemons = Array.from({ length: 151 }).map((v, i) => `${i + 1}`);
-
-    return static151Pokemons.map(id => ({
-        id: id
+    const static151Pokemons = data.results.map(pokemon => ({
+        name: pokemon.name
     }));
-
-    // return [
-    //     { id: '1' },
-    //     { id: '2' },
-    //     { id: '3' },
-    //     { id: '4' },
-    //     { id: '5' },
-    //     { id: '6' },
-    // ]
+    return static151Pokemons.map(({ name }) => ({
+        name: name
+    }));
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
     try {
-        const { id, name } = await getPokemon(params.id);
+        const { id, name } = await getPokemon(params.name);
 
         return {
             title: `${id} - ${name}`,
@@ -44,9 +38,9 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     }
 }
 
-const getPokemon = async (id: string): Promise<Pokemon> => {
+const getPokemon = async (name: string): Promise<Pokemon> => {
     try {
-        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
             // cache: 'force-cache' // TODO: cambiar esto en un futuro
             next: {
                 revalidate: 60 * 60 * 30 * 6
@@ -64,7 +58,7 @@ const getPokemon = async (id: string): Promise<Pokemon> => {
 
 export default async function PokemonPage({ params }: Props) {
 
-    const pokemon = await getPokemon(params.id);
+    const pokemon = await getPokemon(params.name);
 
 
     return (
